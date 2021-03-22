10960a922c6aedb5234ec6e3ebfb22bfda7202e3
MarceloRDP <marcelinho.reis@yahoo.com.br>
2021-03-22 20:31:38 -0300

    Novo upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function enviarFormulario() {
+    var nomecliente = document.getElementById(`nomecliente`).nodeValue;
+    var tipocachorro = document.getElementById(`tipocachorro`).nodeValue;
+    var nomecachorro = document.getElementById(`nomecachorro`).nodeValue;
+
+    return false;
+}
