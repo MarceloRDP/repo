cec43aa61a6298b552d5c8eafd0c628ac34700fc
MarceloRDP <marcelinho.reis@yahoo.com.br>
2021-03-26 20:25:31 -0300

    Novo upload

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
-function enviarFormulario() {
-    var nomecliente = document.getElementById(`nomecliente`).nodeValue;
-    var tipocachorro = document.getElementById(`tipocachorro`).nodeValue;
-    var nomecachorro = document.getElementById(`nomecachorro`).nodeValue;
+// function enviarFormulario() {
+//     var nomecliente = document.getElementById(`nomecliente`).nodeValue;
+//     var tipocachorro = document.getElementById(`tipocachorro`).nodeValue;
+//     var nomecachorro = document.getElementById(`nomecachorro`).nodeValue;
+
+//     return false;
+// }
+
+// function enviarFormulario(formulario) {   
+//     var linha = "str";
+
+//     for(i=0; i<formulario.length-1; i++) {
+//         linha += "<td>" + formulario[i].value + "<td>";
+//     }
+
+//     linha += "</tr>";
+
+//     document.getElementsByTagName("tbody")[0].innerHTML += linha;
+
+//  document.getElementsByTagName("tbody")[0].innerHTML += linha;
+//     "<tr>\
+//         <td>\
+//             " + formulario.nomecliente.value + "\
+//         </td>\
+//         <td>\
+//             " + formulario.tipocachorro.value + "\
+//         </td>\
+//         <td>\
+//             " + formulario.tipocachorro.value + "\
+//         </td>\
+//     </tr>"
+
+ document.querySelector("table").setAttribute(`style`, `display: inline-block`);
+ document.getElementsByTagName("tbody")[0].innerHTML += linha;
+    `<tr>\
+        <td>\
+            ${formulario.nomecliente.value}
+        </td>\
+        <td>\
+            ${formulario.tipocachorro.value}
+        </td>\
+        <td>\
+            ${formulario.nomecachorro.value}
+        </td>\
+    </tr>`
 
     return false;
 }
+
